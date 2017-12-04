46ebf4729938f00578fa2dd4b9c11d3666c98ab0
Sanan Mammadov <sananmammadov99@gmail.com>
2017-12-04 09:09:29 -0800

    Initial commit

---
+++ Assets/CubeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	public float itme;
+	public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Mouse0)) {
+			//Debug.Log ("Hello");
+		}
+	}
+
+	void OnMouseDown(){
+		rb.AddForce (new Vector3(0,1,0) * itme,ForceMode.VelocityChange);
+	}
+}
